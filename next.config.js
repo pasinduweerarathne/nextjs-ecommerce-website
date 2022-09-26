0730be00f3c0a95662c91c24dbd6209b314a430e
H.K.D.Pasindu Pramod Weerarathne <pasiweerarathne98@gmail.com>
2022-09-26 11:10:08 +0530

    add register,signin and navbar

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    BASE_URL: "http://localhost:3000",
+    MONGODB_URL:
+      "mongodb+srv://pasindu:pasi1998@cluster0.a7imgjv.mongodb.net/ecom_DB?retryWrites=true&w=majority",
+  },
+};
