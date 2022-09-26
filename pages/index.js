7cbf875c95ddfed987aa9855876ebf3d275f8e80
H.K.D.Pasindu Pramod Weerarathne <pasiweerarathne98@gmail.com>
2022-09-27 00:28:48 +0530

    add product component and api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import { getData } from "../utils/fetchData";
+import Head from "next/head";
+import ProductItem from "../components/ProductItem";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = (props) => {
+  const [products, setProducts] = useState(props.productProps);
+
+  return (
+    <div className="products">
+      <Head>
+        <title>Home Page</title>
+      </Head>
+
+      {products.length === 0 ? (
+        <h2>No Products</h2>
+      ) : (
+        products.map((product) => (
+          <ProductItem key={product._id} product={product} />
+        ))
+      )}
+    </div>
+  );
 };
+
+export async function getServerSideProps() {
+  const res = await getData("product");
+
+  return {
+    props: {
+      productProps: res.products,
+      result: res.result,
+    },
+  };
+}
 
 export default Home;
